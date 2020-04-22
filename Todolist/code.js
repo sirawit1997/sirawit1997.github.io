09d9b308b0015d722da3b55399c194e76ecd02ab
sirawit1997 <Sirawi.u@ku.th>
2020-04-23 00:33:55 +0700

    Add mybeat.html + login + register

--- Todolist/code.js
+++ Todolist/code.js
@@ -12,27 +12,68 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 var Firebase = firebase.firestore();
-
-Firebase.collection("todos").get().then(function(querySnapshot) {
-    var str = ''
-    querySnapshot.forEach(function(doc) {
-        // doc.data() is never undefined for query doc snapshots
-        str += '<tr>'+
-                '<td>' + doc.data().title + '</td>' +
-                '<td>' + doc.data().artist + '</td>' +
-                '<td>' + doc.data().gene + '</td>' +
-                '<td>' + doc.data().time + '</td>' +
-                '<td><a href="show.html?id='+doc.id+'" class="btn btn-warning">Show</a> &nbsp; <button onclick="onDelete(\''+doc.id+'\')" class="btn btn-danger">Delete</button></td>'+
-                '</tr>';
-                
+var currentUser = null ;
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      console.log(user);
+      Firebase.collection("todos").get().then(function(querySnapshot) {
+        var str = ''
+        querySnapshot.forEach(function(doc) {
+            // doc.data() is never undefined for query doc snapshots
+            str += '<tr>'+
+                    '<td>' + doc.data().title + '</td>' +
+                    '<td>' + doc.data().artist + '</td>' +
+                    '<td>' + doc.data().gene + '</td>' +
+                    '<td>' + doc.data().time + '</td>' +
+                    '<td><a href="show.html?id='+doc.id+'" class="btn btn-warning">Show</a> &nbsp; <button onclick="onDelete(\''+doc.id+'\')" class="btn btn-danger">Delete</button></td>'+
+                    '</tr>';
+                    
+        });
+        document.getElementById('tbody').innerHTML = str;
+    });    
+      currentUser = user;
+      document.getElementById('authen').innerHTML= '<li class="nav-item"><a class="nav-link">'+currentUser.email+'</a></li><li class="nav-item"><a class="nav-link" data-toggle="tooltip" href="Login.html" onclick="onLogout(event)">Logout</a></li>'
+    } else {
+      // No user is signed in.
+      console.log("No one Login");
+      Firebase.collection("todos").get().then(function(querySnapshot) {
+        var str = ''
+        querySnapshot.forEach(function(doc) {
+            // doc.data() is never undefined for query doc snapshots
+            str += '<tr>'+
+                    '<td>' + doc.data().title + '</td>' +
+                    '<td>' + doc.data().artist + '</td>' +
+                    '<td>' + doc.data().gene + '</td>' +
+                    '<td>' + doc.data().time + '</td>' +
+                    '<td><a href="show.html?id='+doc.id+'" class="btn btn-warning">Show</a></td>'+
+                    '</tr>';
+                    
+        });
+        document.getElementById('tbody').innerHTML = str;
     });
-    document.getElementById('tbody').innerHTML = str;
-});
-
+    
+    }
+  });
 function onDelete(id){
     Firebase.collection("todos").doc(id).delete().then(function() {
         window.location.href = "index.html";
     }).catch(function(error) {
         console.error("Error removing document: ", error);
     });
+}
+
+function onLogout(e){
+    e.preventDefault();
+    firebase.auth().signOut().then(function() {
+        // Sign-out successful.
+        window.location.href = "index.html";
+      }).catch(function(error) {
+        // An error happened.
+      });
+}
+
+function onLogin(e){
+    e.preventDefault();
+
 }
---
+++ Todolist/register.js
@@ -0,0 +1,26 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDxd3pNt3jc_TkiLIEiijNVjYZbmwk51kQ",
+    authDomain: "todolist-16533.firebaseapp.com",
+    databaseURL: "https://todolist-16533.firebaseio.com",
+    projectId: "todolist-16533",
+    storageBucket: "todolist-16533.appspot.com",
+    messagingSenderId: "651993545337",
+    appId: "1:651993545337:web:231c216681ad0e40cbb2a8",
+    measurementId: "G-74ZX47DVZ9"
+};
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var Firebase = firebase.firestore();
+
+  function onSubmit(e){
+      e.preventDefault();
+      var email = document.getElementById('usr').value;
+      var password = document.getElementById('pwd').value
+      firebase.auth().createUserWithEmailAndPassword(email, password).then(function(){ window.location.href ="index.html"}).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+      });
+  }
